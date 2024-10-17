6868ef68f74820a78e39efe4de4df0df2b9a4c59
dangvanthua <thuandang021102@gmail.com>
2024-10-17 13:15:16 +0700

    update file

--- front/3098/3098.php
+++ front/3098/3098.php
@@ -42,128 +42,134 @@ $less->compileFile('less/3098.less', 'css/3098.css');
             <button><i class="fas fa-paper-plane"></i></button>
         </div>
     </div>
-    <div class="container footer">
-        <div class="row">
-            <div class="col-lg-3 col-md-6">
-                <img alt="Repa logo" class="mb-3" height="50" src="./images/white-logo.png" width="100">
-                <p>
-                    Lorem ipsum dolor sit amet, conse ctetur adip iscing elit. Nt viverra eros euis mod Vestibu diam
-                    suspendisse adi pisc diam quis. Et id volutpat sit odio.
-                </p>
-                <div class="social-icons">
-                    <a href="#">
-                        <i class="fab fa-facebook-f">
-                        </i>
-                    </a>
-                    <a href="#">
-                        <i class="fab fa-twitter">
-                        </i>
-                    </a>
-                    <a href="#">
-                        <i class="fab fa-instagram">
-                        </i>
-                    </a>
-                    <a href="#">
-                        <i class="fab fa-pinterest">
-                        </i>
-                    </a>
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <img alt="Repa logo" class="mb-3" height="50" src="./images/white-logo.png" width="100">
+                    <p>
+                        Lorem ipsum dolor sit amet, conse ctetur adip iscing elit. Nt viverra eros euis mod Vestibu diam
+                        suspendisse adi pisc diam quis. Et id volutpat sit odio.
+                    </p>
+                    <div class="social-icons">
+                        <a href="#">
+                            <i class="fab fa-facebook-f">
+                            </i>
+                        </a>
+                        <a href="#">
+                            <i class="fab fa-twitter">
+                            </i>
+                        </a>
+                        <a href="#">
+                            <i class="fab fa-instagram">
+                            </i>
+                        </a>
+                        <a href="#">
+                            <i class="fab fa-pinterest">
+                            </i>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5>
+                        Services
+                    </h5>
+                    <ul>
+                        <li>
+                            <i class="fas fa-square">
+                            </i>
+                            PC Repair
+                        </li>
+                        <li>
+                            <i class="fas fa-square">
+                            </i>
+                            Laptop Repair
+                        </li>
+                        <li>
+                            <i class="fas fa-square">
+                            </i>
+                            Smartphone Repair
+                        </li>
+                        <li>
+                            <i class="fas fa-square">
+                            </i>
+                            Tablet Repair
+                        </li>
+                        <li>
+                            <i class="fas fa-square">
+                            </i>
+                            Mac Repair
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5>
+                        Useful Links
+                    </h5>
+                    <ul>
+                        <li>
+                            <i class="fas fa-square">
+                            </i>
+                            Conditions
+                        </li>
+                        <li>
+                            <i class="fas fa-square">
+                            </i>
+                            Privacy Policy
+                        </li>
+                        <li>
+                            <i class="fas fa-square">
+                            </i>
+                            Latest Blogs
+                        </li>
+                        <li>
+                            <i class="fas fa-square">
+                            </i>
+                            Testimonials
+                        </li>
+                        <li>
+                            <i class="fas fa-square">
+                            </i>
+                            Our Mechanics
+                        </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <h5>
+                        Get In Touch
+                    </h5>
+                    <ul>
+                        <li class="flex-touch">
+                            <i class="fas fa-phone icon-touch">
+                            </i>
+                            <div>
+                                <span class="touch-head">Phone</span>
+                                <span>+123 456 778</span>
+                            </div>
+                        </li>
+                        <li class="flex-touch">
+                            <i class="fas fa-envelope icon-touch">
+                            </i>
+                            <div>
+                                <span class="touch-head">Email</span>
+                                <span>info@repa.com</span>
+                            </div>
+                        </li>
+                        <li class="flex-touch">
+                            <i class="fas fa-map-marker-alt icon-touch">
+                            </i>
+                            <div>
+                                <span class="touch-head">Location</span>
+                                <span>Coral Way, Miami, Florida, 33169</span>
+                            </div>
+                        </li>
+                    </ul>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6">
-                <h5>
-                    Services
-                </h5>
-                <ul>
-                    <li>
-                        <i class="fas fa-square">
-                        </i>
-                        PC Repair
-                    </li>
-                    <li>
-                        <i class="fas fa-square">
-                        </i>
-                        Laptop Repair
-                    </li>
-                    <li>
-                        <i class="fas fa-square">
-                        </i>
-                        Smartphone Repair
-                    </li>
-                    <li>
-                        <i class="fas fa-square">
-                        </i>
-                        Tablet Repair
-                    </li>
-                    <li>
-                        <i class="fas fa-square">
-                        </i>
-                        Mac Repair
-                    </li>
-                </ul>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h5>
-                    Useful Links
-                </h5>
-                <ul>
-                    <li>
-                        <i class="fas fa-square">
-                        </i>
-                        Conditions
-                    </li>
-                    <li>
-                        <i class="fas fa-square">
-                        </i>
-                        Privacy Policy
-                    </li>
-                    <li>
-                        <i class="fas fa-square">
-                        </i>
-                        Latest Blogs
-                    </li>
-                    <li>
-                        <i class="fas fa-square">
-                        </i>
-                        Testimonials
-                    </li>
-                    <li>
-                        <i class="fas fa-square">
-                        </i>
-                        Our Mechanics
-                    </li>
-                </ul>
-            </div>
-            <div class="col-lg-3 col-md-6">
-                <h5>
-                    Get In Touch
-                </h5>
-                <ul>
-                    <li class="flex-touch">
-                        <i class="fas fa-phone icon-touch">
-                        </i>
-                        <div>
-                            <span class="touch-head">Phone</span>
-                            <span>+123 456 778</span>
-                        </div>
-                    </li>
-                    <li class="flex-touch">
-                        <i class="fas fa-envelope icon-touch">
-                        </i>
-                        <div>
-                            <span class="touch-head">Email</span>
-                            <span>info@repa.com</span>
-                        </div>
-                    </li>
-                    <li class="flex-touch">
-                        <i class="fas fa-map-marker-alt icon-touch">
-                        </i>
-                        <div>
-                            <span class="touch-head">Location</span>
-                            <span>Coral Way, Miami, Florida, 33169</span>
-                        </div>
-                    </li>
-                </ul>
-            </div>
+        </div>
+
+        <div class="copyright">
+            <p class="copyright-text">Copyright @2024. Repa All Rights Reserved By <span>HiBootstrap</span></p>
         </div>
     </div>
 </body>
